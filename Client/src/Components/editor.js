154157b2d449728fadf1b65d3cb087464e6711ae
billalkhn <billalkhn98@gmail.com>
2021-05-11 18:16:42 +0500

    done

--- Client/src/Components/editor.js
+++ Client/src/Components/editor.js
@@ -4,25 +4,30 @@ import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 var Algorithmia = require("algorithmia");
 
 const Editor = () => {
-  const [PreduictData, setPredictData] = useState([]);
+  const [PredictData, setPredictData] = useState([]);
+  const [data] = useState(["<p>Here we will use the data and also recommend or predict some words to complete the sentence</p>"]);
 
+  var firstWords = [];
+  
   const handleKeyPress = (e) => {
     if (e.key === 'Tab' && e.shiftKey) {         
       console.log("Detected Shift+Enter")
+      for (var i=0;i<codelines.length;i++)
+      {
+        var words = PredictData[i].split(" ");
+        firstWords.push(words[0]);
+      }
     }
     else if (e.keyCode === 27) {         
       console.log("Detected ESC")
+      setPredictData('');
     }
     else if (e.key === "Tab" || e.keyCode === 9) {         
       console.log("Detected Tab")
+      data.push(PredictData)
+      
     }
-    
-    
   }
-
-
-
-
 
 
 
@@ -36,7 +41,7 @@ const Editor = () => {
       });
   });
 
-  console.log(PreduictData);
+  console.log(PredictData);
 
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
